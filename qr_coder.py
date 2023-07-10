596a05c3a32875cf7be938acd0bc0a16d012c19b
OneJessica <janezhangbest@outlook.com>
2023-07-10 22:32:29 +0800

    add qr_coder

---
+++ qr_coder.py
@@ -0,0 +1,21 @@
+import qrcode
+import streamlit as st
+st.title('二维码生成器')
+def get_qr(text,filename,fill='black',color = 'white'):
+    qr = qrcode.QRCode(version=1,box_size=10,border=5)
+    qr.add_data(text)
+    qr.make(fit = True)
+    img = qr.make_image(fill_color=fill,back_color=color)
+    img.save(filename)
+with st.form('qr_coder'):
+    text = st.text_input('二维码内容')
+    color = st.color_picker(label='背景颜色',value = '#f5f7f5')
+    fill = st.color_picker(label='填充颜色',)
+    filename = 'text.png'
+
+    if st.form_submit_button('生成'):
+        if not text:
+            st.error('请输入二维码内容')
+            st.stop()
+        get_qr(text,filename,fill=fill,color=color)
+        st.image(filename)
